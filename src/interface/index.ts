f90eb3bc5d63e9d9bf9e695c3b5df4ccadf062c0
Oleksii Vovnenko <kespa71for@gmail.com>
2024-03-09 17:14:24 +0100

    add interface for Button

--- src/interface/index.ts
+++ src/interface/index.ts
@@ -1,3 +1,11 @@
+import React from "react";
+
 export interface ILogo {
+    children: string
+}
+
+export interface IButton extends
+    React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>, React.AriaAttributes
+{
     children: string
 }
