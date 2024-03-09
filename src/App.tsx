2b08194823c2025239c163aad1db612c86aff364
Oleksii Vovnenko <kespa71for@gmail.com>
2024-03-09 17:18:14 +0100

    import page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Header from "./components/Header/Header.tsx";
+import Page from "./pages/Page.tsx";
 
 
 function App() {
-  return (
-    <>
-      <Header />
-    </>
-  )
+    return (
+        <>
+            <Header/>
+            <Page/>
+        </>
+    )
 }
 
 export default App
