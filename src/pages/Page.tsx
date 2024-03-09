35731deb487a1fad98c2810c09d4af18f1781eac
Oleksii Vovnenko <kespa71for@gmail.com>
2024-03-09 17:18:02 +0100

    add page

---
+++ src/pages/Page.tsx
@@ -0,0 +1,16 @@
+import {FC} from 'react';
+
+import style from "./Page.module.css"
+const Page:FC = () => {
+    return (
+        <section className={style.page}>
+            <div className="container">
+                <div className={style.wrapper}>
+
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Page;
