bc0a86d7c18f5f8036773086c53a2ca0d9ecc4e3
Oleksii Vovnenko <kespa71for@gmail.com>
2024-03-10 14:29:42 +0100

    update components Form: add functional post query form

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,15 +1,64 @@
-import {FC} from "react";
-import Input from "../UI/Input/Input.tsx";
-import Button from "../UI/Button/Button.tsx";
+import {FC, useState} from "react";
+import {SubmitHandler, useForm} from "react-hook-form";
+import {useMutation} from "react-query";
+
+import {queryClient} from "../../Provider.tsx";
+import {IFormInput} from "../../interface";
+import {worksServices} from "../../services/items.services.ts";
 
 import styles from "./Form.module.css";
+
 const Form:FC = () => {
+    const [imageBase64, setImageBase34] = useState<string>("")
+    const {register, handleSubmit} = useForm<IFormInput>()
+    const onSubmit:SubmitHandler<IFormInput> = (data) => {
+        mutation.mutate({...data, img: imageBase64});
+    };
+    const handlerFileChange = (event:any) => {
+        const file = event.target.files[0];
+        const reader = new FileReader();
+
+        reader.onloadend = () => {
+            setImageBase34(reader.result);
+        };
+
+        reader.readAsDataURL(file);
+    }
+
+    const mutation = useMutation((formData) => worksServices.postWork(formData), {
+        onSuccess: () => {
+            queryClient.invalidateQueries("items");
+        }
+    });
+
     return (
-        <form className={styles.form}>
-            <Input placeholder={"Title"} type={"text"}/>
-            <Input placeholder={"Link"} type={"text"}/>
-            <Input type={"file"}/>
-            <Button children={"Create"}/>
+        <form
+            className={styles.form}
+            onSubmit={handleSubmit(onSubmit)}
+        >
+            <input
+                placeholder={"Title"}
+                type={"text"}
+                {...register('title')}
+            />
+            <input
+                placeholder={"Link"}
+                type={"text"}
+                {...register("link")}
+            />
+            <input
+                placeholder={"Category"}
+                type={"text"}
+                {...register("category")}
+            />
+            <input
+                type={"file"}
+                onChange={handlerFileChange}
+            />
+            <button
+                children={"Create"}
+                type={"submit"}
+            />
         </form>
     );
 };
