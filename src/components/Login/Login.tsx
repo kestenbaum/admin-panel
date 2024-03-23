a4af3ab82d5c513831dbe1ec0227cdd4abb426d2
Oleksii Vovnenko <kespa71for@gmail.com>
2024-03-23 13:49:37 +0100

    update Login:delete custom input

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,5 +1,4 @@
 import {FC} from "react";
-import Input from "../UI/Input/Input.tsx";
 import Button from "../UI/Button/Button.tsx";
 
 import styles from "./Login.module.css"
@@ -7,9 +6,9 @@ const Login:FC = () => {
     return (
         <form className={styles.form}>
             <label className={styles.label} htmlFor="name">Username:</label>
-            <Input type={"text"} id={"name"} name={"name"}/>
+            <input type={"text"} id={"name"} name={"name"}/>
             <label className={styles.label} htmlFor="pass">Password:</label>
-            <Input type={"password"} id={"pass"} name={"pass"}/>
+            <input type={"password"} id={"pass"} name={"pass"}/>
             <Button children={"Login"}/>
         </form>
     );
