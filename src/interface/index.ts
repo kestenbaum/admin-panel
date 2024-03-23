9a56dba73c5fcc6876c400f1c0c9b40f33bd557c
Oleksii Vovnenko <kespa71for@gmail.com>
2024-03-23 13:47:04 +0100

    update all interface

--- src/interface/index.ts
+++ src/interface/index.ts
@@ -1,40 +1,39 @@
-import React, {InputHTMLAttributes} from "react";
-export interface ILogo {
+import React, {AriaAttributes, ButtonHTMLAttributes, DetailedHTMLProps, ReactNode} from "react";
+
+//Providers
+export interface ProviderProps {
+    children: React.ReactNode
+}
+
+//INTERFACE UI
+export interface ButtonProps extends DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>, AriaAttributes {
     children: string
+    onClick?: () => void
 }
 
-export interface IButton extends
-    React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>, React.AriaAttributes
-{
-    children: string
-    color?: "red" | "green"
-}
-
-export interface IInput  extends InputHTMLAttributes<HTMLInputElement>{}
-
-export interface Icons {
-    _id?: string,
-    title: string,
-    img: string,
-    category?: string
-    link?: string
-}
-
-export interface IGetData {
-    status: number
-    data: Icons[]
-}
-
-export interface IPopup {
-    children: React.ReactNode,
+export interface PopupProps {
+    children: ReactNode,
     showModal: boolean,
     setShowModal: any
 }
 
-export interface IFormInput {
+//INTERFACE FORM
+export interface IForm {
+    _id?: string
     title: string
     link: string,
-    img: string | null | ArrayBuffer,
+    img?: string | null | ArrayBuffer,
     category: string
+}
+
+export interface FormProps {
+    showModal?: boolean,
+    setShowModal: any
+}
+
+//Data
+export interface GetDataProps {
+    status: number
+    data: IForm[]
 }
 
