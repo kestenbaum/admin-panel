15ca36cd016f1211f4848d6abc7aada414d6071a
Oleksii Vovnenko <kespa71for@gmail.com>
2024-03-23 13:51:25 +0100

    update List: add popup

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,11 +1,16 @@
-import {FC} from "react";
+import {FC, useState} from "react";
 import ListItem from "../ListItem/ListItem.tsx";
 import {worksServices} from "../../services/items.services.ts";
 
 import {useQuery} from "react-query";
-import styles from "./List.module.css";
+
+import styles from "./List.module.css"
+import Popup from "../UI/Popup/Popup.tsx";
+import Form from "../Form/Form.tsx";
 
 const List: FC = () => {
+    const [modal, setModal] = useState(false)
+    const toggle = () => setModal(!modal)
     const {data, isLoading} = useQuery({
         queryKey: ["items"],
         queryFn: () => worksServices.getWorks()
@@ -13,15 +18,26 @@ const List: FC = () => {
 
     return (
         <div className={styles.wrapper}>
+            <Popup
+                showModal={modal}
+                setShowModal={toggle}
+                children={<Form setShowModal={setModal} showModal={modal}/>}
+            />
+            <div
+                className={styles.create__form}
+                onClick={() => setModal(!modal)}
+            >
+                Create Element
+            </div>
             {!isLoading
                 ? data
                     ?.map(item =>  <ListItem
-                        key={item._id}
                         _id={item._id}
+                        key={item._id}
                         title={item.title}
                         img={item.img}
                         link={item.link}
-                    />)
+                        category={item.category}/>)
                 :
                 <>loader</>}
         </div>
