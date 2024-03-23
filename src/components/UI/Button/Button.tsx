365cd031990090da97ee950ac16e81a1a9f86ff1
Oleksii Vovnenko <kespa71for@gmail.com>
2024-03-23 13:45:03 +0100

    update component Button

--- src/components/UI/Button/Button.tsx
+++ src/components/UI/Button/Button.tsx
@@ -1,9 +1,9 @@
 import {FC} from 'react';
-import {IButton} from "../../../interface";
+import {ButtonProps} from "../../../interface";
 
 import style from "./Button.module.css";
 
-const Button:FC<IButton> = ({children, ...rest}) => {
+const Button:FC<ButtonProps> = ({children, ...rest}) => {
     return (
         <button className={style.btn} {...rest}>
             {children}
