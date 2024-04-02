524b367117110b5e6199a3278461ddfe8ec73f4c
Oleksii Vovnenko <kespa71for@gmail.com>
2024-04-02 16:47:25 +0200

    fix component: main

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import { Provider } from './Provider.tsx'
+
 
 import './index.css'
+import { Provider } from './Provider'
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider>
